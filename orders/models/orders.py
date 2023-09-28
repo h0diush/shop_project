7465bc423771f11109aef8510760b671493382f2
h0diush <h0diush@yandex.ru>
2023-09-28 21:04:07 +0300

    Advanced administrator functionality

--- orders/models/orders.py
+++ orders/models/orders.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from shop.models import Product
@@ -11,6 +12,8 @@ class Order(models.Model):
     postal_code = models.CharField("Индекс", max_length=20)
     city = models.CharField("Город", max_length=75)
     paid = models.BooleanField("Оплачено", default=False)
+    stripe_id = models.CharField("Stripe идентификатор", max_length=250,
+                                 blank=True)
     created = models.DateTimeField("Создано", auto_now_add=True)
     updated = models.DateTimeField("Изменено", auto_now=True)
 
@@ -24,6 +27,18 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Заказ № {self.pk}"
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+    def get_stripe_url(self):
+        if not self.stripe_id:
+            return ''
+        if '_test_' in settings.STRIPE_SECRET_KEY:
+            path = '/test/'
+        else:
+            path = '/'
+        return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
 
 
 class OrderItem(models.Model):
--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from orders.views import order_create
+from orders.views import order_create, order_admin
 
 urlpatterns = [
     path('orders/', order_create.OrderCreateView.as_view(),
-         name='order_create')
+         name='order_create'),
+    path('admin/order/<int:order_id>/', order_admin.admin_order_detail,
+         name='admin_order_detail')
 ]
